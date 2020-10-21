1a8858982544101030de32130dd6a68319696a67
nichoth <nichoth@gmail.com>
2020-10-21 11:16:35 -0700

    test runs

--- test/create-server.js
+++ test/create-server.js
@@ -3,6 +3,7 @@ var caps = require('ssb-caps')
 var ssbConfigInject = require('ssb-config/inject')
 var home = require('user-home')
 var path = require('path')
+var ssbKeys = require('ssb-keys')
 
 function start () {
     var appName = 'ssb-TEST-' + Math.random()
@@ -10,13 +11,14 @@ function start () {
     process.on('exit', function () {
         rimraf.sync(path.join(home, '.' + appName))
         console.log('deleted', appName)
-    }
+    })
 
+    var opts = { caps }
     var config = ssbConfigInject(appName, opts)
     var keyPath = path.join(config.path, 'secret')
     config.keys = ssbKeys.loadOrCreateSync(keyPath)
 
-    var sbot = Sbot()
+    var sbot = Sbot
         .use(require('../')({ postType: 'post' }))
         .call(null, config)
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,19 @@
 var test = require('tape')
+var createSbot = require('./create-server')
+
+var sbot = createSbot()
 
 test('stuff', function (t) {
+    sbot.publish({
+        type: 'post',
+        text: 'Hello, world! #my-tag ok #tag-again'
+    }, function (err, msg) {
+        // console.log('**post 2', err, msg)
+        if (err) throw err
+        sbot.tags.get(function (err, data) {
+            console.log('**get**', err, data)
+            t.end()
+        })
+    })
 })
 
